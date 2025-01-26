396cd6cca14beab00d2f6492a9160c77f690ea59
Alisam-alkhalil <alisamalkhalil99@gmail.com>
2025-01-26 17:12:52 +0000

    Fixed viewCounter

--- viewCounter.js
+++ viewCounter.js
@@ -1,52 +1,41 @@
-
 AWS.config.update({
-    region: "eu-west-1", 
+    region: "eu-west-1",
     credentials: new AWS.CognitoIdentityCredentials({
         IdentityPoolId: "eu-west-1:64f1cd95-58cf-4e40-b40a-0f6511a6d1c2",
     }),
 });
 
-
 const docClient = new AWS.DynamoDB.DocumentClient();
-
 
 function getViewCount() {
     const params = {
-        TableName: "CVViewCount",   
+        TableName: "CVViewCount",  
+        Key: {
+            "views": "viewcount"  
+        }
     };
 
-
-    docClient.scan(params, function(err, data) {
+    docClient.get(params, function(err, data) {
         if (err) {
             console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
             document.getElementById("view-count").innerText = "Error loading view count";
         } else {
-            console.log("DynamoDB Scan Result:", JSON.stringify(data, null, 2)); 
+            console.log("DynamoDB Get Item Result:", JSON.stringify(data, null, 2));
 
-            if (data.Items && data.Items.length > 0) {
-               
-                const firstItem = data.Items[0]; 
-                console.log("First item:", JSON.stringify(firstItem, null, 2)); 
-
-                if (firstItem.views) {
-                    const viewCount = parseInt(firstItem.views, 10);  
-                    if (!isNaN(viewCount)) {
-                        document.getElementById("view-count").innerText = viewCount || "View count not found";
-                    } else {
-                        console.error("Invalid view count value.");
-                        document.getElementById("view-count").innerText = "View count not found";
-                    }
+            if (data.Item && data.Item.count) {  
+                const viewCount = parseInt(data.Item.count, 10); 
+                if (!isNaN(viewCount)) {
+                    document.getElementById("view-count").innerText = viewCount || "View count not found";
                 } else {
-                    console.error("View count not found in the first item.");
+                    console.error("Invalid view count value.");
                     document.getElementById("view-count").innerText = "View count not found";
                 }
             } else {
-                console.error("No items found in DynamoDB.");
+                console.error("Count not found in the item.");
                 document.getElementById("view-count").innerText = "View count not found";
             }
         }
     });
 }
-
 
 window.onload = getViewCount;
